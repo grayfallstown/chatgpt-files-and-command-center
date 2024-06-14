5832b56831a51c6a175151e119a74f730545850a
grayfallstown <grayfallstown@protonmail.com>
2024-06-14 18:06:04 +0200

    fixes CommandController not showing projectID as parameter in openapi schema

--- src/main/java/net/grayfallstown/chatgptfileandcommandcenter/command/CommandController.java
+++ src/main/java/net/grayfallstown/chatgptfileandcommandcenter/command/CommandController.java
@@ -16,6 +16,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
@@ -39,6 +40,7 @@ public class CommandController {
     @PostMapping
     public ResponseEntity<StreamingResponseBody> executeCommand(
             @AuthenticationPrincipal ProjectConfig projectConfig,
+            @RequestParam(required = true) String projectID,
             @Valid @RequestBody CommandRequest commandRequest) {
         if (!projectConfig.isExecuteCommands()) {
             throw new CommandsDisabledException("Command Execution disabled in project '" +
